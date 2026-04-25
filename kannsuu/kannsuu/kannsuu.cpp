c1ba6d79fb70e03e565e2357547e71542f905e8c
Milin00 <wtnbryt1122@gmail.com>
2026-04-25 16:30:34 +0900

    A

---
+++ kannsuu/kannsuu/kannsuu.cpp
@@ -0,0 +1,19 @@
+﻿#include<stdio.h>
+
+int loop(int a)
+{
+	int a = 0;
+	scanf("%d", &a);
+	int answer = 1;
+	for (int i = 0; i < 5; i++)
+	{
+		answer *= answer;
+	}
+	return answer;
+}
+
+int main()
+{
+	int x, y, z;
+	int answer = loop(x) * loop(y) * loop(z);
+}
